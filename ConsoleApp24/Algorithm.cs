25cbfffc17fb60ccdc5cec7f0bbba53ff3e68fb2
Артём Коваленко <ak7527203@gmail.com>
2022-12-17 18:39:21 +0200

    Add project files.

---
+++ ConsoleApp24/Algorithm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    internal class Algorithm
+    {
+        private int n;
+        private int x;
+        
+        public Algorithm(int n, int x)
+        {
+            this.n = n;
+            this.x = x;
+        }
+        public int N 
+        { 
+            get { return n; } 
+            set { if (n > 0) n = value; }
+        }
+        public int X
+        {
+            get { return x; }
+        }
+
+        public double GetAlgorithm(double K = 1)
+        {
+            for (int i = 1; i < N + 1; i++)
+            {
+                double Factorial = 1;
+                int FactorialMult = 1;
+                do
+                {
+                    Factorial *= FactorialMult;
+                    FactorialMult++;
+                } while (FactorialMult <= i);
+                K += ((i * i + 1) / Factorial) * Math.Pow((X / 2), i);
+            }
+            return K;
+        }
+    }
+}
---
+++ ConsoleApp24/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp24;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество рядов алгоритма: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите x: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Algorithm algorithm = new(n, x);
+        Console.WriteLine(algorithm.GetAlgorithm());
+    }
+}
